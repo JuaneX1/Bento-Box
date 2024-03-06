727b7d7e81d9266b388c23a88d203b6ec9d32e4c
Juan <penuelajuanesteban30@gmail.com>
2024-03-06 11:21:24 -0500

    Made first change

---
+++ src/components/BentoLogo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const BentoLogo = () => {
+    return (
+        <div>
+            <h1>BENTO BOX!</h1>
+            <img src='./images/bentologo.png' height={300} width={300} alt='Bento Logo' />
+        </div>
+    );
+}
+
+export default BentoLogo;
