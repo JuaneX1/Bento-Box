cd1a0f43b4091232d165e7f7b827b2daa5d7f9e7
Ronit1120 <ronit.mahalmani@gmail.com>
2024-03-07 23:10:22 -0500

    added login and register api endpoints for local deployment

--- server.js
+++ server.js
@@ -81,3 +81,31 @@ app.post('/api/login', async (req, res, next) => {
         res.status(500).json({ error: 'Internal server error' });
     }
 });
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept,Authorization'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PATCH, DELETE, OPTIONS'
+    );
+    next();
+});
+/*
+// For Heroku deployment
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('frontend/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    });
+}
+*/
+app.listen(PORT, () => {
+    console.log('Server listening on port ' + PORT);
+});
+
